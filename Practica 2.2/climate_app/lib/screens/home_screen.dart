db61ed6aedd1ec4661036e01c74508ce14adec26
TwistableDoor73 <jesusefrainbm29@gmail.com>
2026-08-01 17:36:29 -0600

    Practica 2.2

---
+++ Practica 2.2/climate_app/lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'search_screen.dart';
+import '../widgets/weather_icon.dart';
+class HomeScreen extends StatelessWidget {
+const HomeScreen({Key? key}) : super(key: key);
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: const Text('Clima Actual'),
+centerTitle: true,
+),
+body: Center(
+child: Column(
+mainAxisAlignment: MainAxisAlignment.center,
+children: [
+const Text(
+'24°C',
+style: TextStyle(fontSize: 72, fontWeight: FontWeight.bold, color: Colors.blue),
+),
+const SizedBox(height: 16),
+const Text(
+'Santiago de Querétaro',
+style: TextStyle(fontSize: 24),
+),
+const SizedBox(height: 32),
+WeatherIcon(condition: 'rainy',),
+const SizedBox(height: 32),
+const Text('Humedad: 65% | Viento: 12 km/h'),
+const SizedBox(height: 40),
+ElevatedButton(
+onPressed: () {
+Navigator.push(
+context,
+MaterialPageRoute(builder: (context) => SearchScreen()),
+);
+},
+child: const Text('Buscar Ciudades'),
+),
+],
+),
+),
+);
+}
+}
